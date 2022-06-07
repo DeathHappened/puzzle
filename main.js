c0d95a069ec0ad3a2c79243063557e2bb5fd4062
DeathHappened <71099208+DeathHappened@users.noreply.github.com>
2022-06-07 08:47:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+var text = ["Welcome fellow player.", "Well, how has life been for you?", "That's great.","Well now that I'm here it's time to make it worse."];
+var i = 0;
+var elemement = document.getElementById("cycle__text");
+
+
+$(document).ready(function() {
+    change()
+});
+
+function change() {
+
+    $("#cycle__text").fadeToggle(5000, 'swing', function() {
+        if ($("#cycle__text").css("display") == "none") {
+            elemement.innerHTML = text[i];
+            i++;
+            if (i >= text.length) {
+                //timeout for the last element
+                setTimeout(function() {
+                    window.location.href = "puzzle.html";
+                }, 3000);
+            }
+        }
+        change();
+    });
+}
